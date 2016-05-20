d4af58ddba7105497d1b843dd6ef889ea6749edf
Robert <dungeonry@hotmail.com>
2016-05-20 06:33:30 -0700

    Added nicknames to messages.

--- routes.js
+++ routes.js
@@ -4,17 +4,14 @@ var path = require("path");
 module.exports = function(server, io){
   server.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "public/views/index.html"));
+  });
 
-    io.on("connection", function(socket){
-      io.emit("notification", "A new chatter has joined");
-
-      socket.on("chatmessage", function(msg){
-        //console.log("message: %s", msg);
-
-        io.emit("chatmessage", msg);
-      });
+  io.on("connection", function(socket){
+    io.emit("notification", "A new chatter has joined");
+    socket.on("chatmessage", function(msg){
+      console.log("message: %s, %s", msg.name, msg.text);
+      io.emit("chatmessage", msg);
     });
-
   });
   //return server;
 };
